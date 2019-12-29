888932002dced73bb875c7fa6f9f50d4560f1c65
UminoAoi <48631554+UminoAoi@users.noreply.github.com>
2019-12-29 20:38:21 +0100

    First commit

---
+++ Assets/Scripts/DragButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragButton : MonoBehaviour, IDragHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        position.z = 0;
+        transform.position = position;
+    }
+}
---
+++ Assets/Scripts/Mask.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mask : MonoBehaviour
+{
+    public GameObject triangle;
+    bool touching;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        touching = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (touching)
+        {
+            triangle.transform.localScale += new Vector3(0.01f, 0.01f);
+            Vector3 rotateVector = new Vector3(0, 0, 25 * Time.deltaTime);
+            triangle.transform.Rotate(rotateVector);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        touching = true;
+    }
+}
