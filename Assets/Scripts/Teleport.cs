76c9154fec1ee5332ac9f1d58897f17e92011034
UminoAoi <48631554+UminoAoi@users.noreply.github.com>
2020-01-10 21:16:21 +0100

    Created teleport

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleport : MonoBehaviour
+{
+    public Scene teleportTo;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(teleportTo.handle);
+    }
+}
